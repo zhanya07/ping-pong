9de480c9b3a65c750c6d33dac36d203f7c54e295
zhanya07 <82751717+zhanya07@users.noreply.github.com>
2021-05-16 11:25:22 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -61,3 +61,41 @@ lose2 = font.render('PLAYER 2 LOSE!', True, (180,0,0))
 
 speed_x = 3
 speed_y = 3
+while game:
+   for e in event.get():
+       if e.type == QUIT:
+           game = False
+  
+   if finish != True:
+       window.fill(back)
+       racket1.update_l()
+       racket2.update_r()
+       ball.rect.x += speed_x
+       ball.rect.y += speed_y
+ 
+       if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+           speed_x *= -1
+           speed_y *= 1
+      
+       #если мяч достигает границ экрана, меняем направление его движения
+       if ball.rect.y > win_height-50 or ball.rect.y < 0:
+           speed_y *= -1
+ 
+       #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+       if ball.rect.x < 0:
+           finish = True
+           window.blit(lose1, (200, 200))
+           game_over = True
+ 
+       #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+       if ball.rect.x > win_width:
+           finish = True
+           window.blit(lose2, (200, 200))
+           game_over = True
+ 
+       racket1.reset()
+       racket2.reset()
+       ball.reset()
+ 
+   display.update()
+   clock.tick(FPS)
